1aed31c4bbc67986b038aeacca552b6bbfa1c86b
Zain-ul-Abideen <ahmza147@gmail.com>
2024-02-18 13:29:11 +0100

    all cases passed

--- kidsWithTheGreatestNumberOfCandies.java
+++ kidsWithTheGreatestNumberOfCandies.java
@@ -7,26 +7,15 @@ public class kidsWithTheGreatestNumberOfCandies {
     }
     public static List<Boolean>  kidsWithCandies(int[] candies, int extraCandies) {
         List<Boolean> result = new ArrayList<Boolean>();
-        Map<Integer, Integer> candiesMap = new HashMap<>();
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i=0; i<candies.length; i++){
-            candiesMap.put(i, candies[i]);
-            arr.add(candies[i]);
-
+        int greatest = 0;
+        for(int i = 0; i < candies.length; i++){
+            greatest = Math.max(candies[i],greatest);
         }
-        int keyCheck;
-        int valueCheck = Integer.MIN_VALUE;
-        for(Map.Entry<Integer,Integer> entry : candiesMap.entrySet()){
-            Integer key = entry.getKey();
-            Integer value = entry.getValue();
-            if((value+extraCandies)>valueCheck){
-                valueCheck =Collections.max(arr);
-                result.add(true);
-            }
-            else {
-                valueCheck = Collections.max(arr);
-                result.add(false);
-            }
+        for (int i=0; i < candies.length; i++){
+            int sum = extraCandies + candies[i];
+            if(sum >= greatest)  result.add(true);
+            else result.add(false);
+
         }
         return result;
     }
