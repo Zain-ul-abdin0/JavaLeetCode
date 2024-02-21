bd38be22dcabb80f9942976c276c22038333092b
Zain-ul-Abideen <ahmza147@gmail.com>
2024-02-21 14:26:42 +0100

    solved

--- IsSubsequence.java
+++ IsSubsequence.java
@@ -3,20 +3,14 @@ public class IsSubsequence {
         System.out.println(isSubsequence("abc","ahbgdc"));
     }
     public static boolean isSubsequence(String s, String t) {
+        if(s.length() == 0) return true;
 
-        char[] c = t.toCharArray();
-        char[] c1 = s.toCharArray();
-        int result = s.length();
-        int count = 0;
-        for(int i = 0; i < c1.length; i++){
-            for(int j = 0; j < c.length; j++){
-                if(c1[i] == c[j]){
-                    count++;
-                }
+        for(int i = 0, j = 0; j < t.length();++j){
+            if(s.charAt(i) == t.charAt(j)){
+                ++i;
             }
+            if(i == s.length()) return true;
         }
-
-        if(result==count) return true;
         return false;
     }
 }
