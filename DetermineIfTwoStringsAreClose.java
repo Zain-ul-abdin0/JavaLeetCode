0834035de0b4191047b30770bba3e122e0931a5b
Zain-ul-Abideen <ahmza147@gmail.com>
2024-02-23 23:50:42 +0100

    Cases Passed DetermineIfTwoStringsAreClose

--- DetermineIfTwoStringsAreClose.java
+++ DetermineIfTwoStringsAreClose.java
@@ -1,37 +1,31 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class DetermineIfTwoStringsAreClose {
     public static void main(String[] args){
         System.out.println(closeStrings( "cabbba", "abbccc"));
     }
     public static boolean closeStrings(String word1, String word2) {
-
-        if(word1.length()==word2.length()){
-
-            char[] charArray = word1.toCharArray();
-            char[] charArray1 = word2.toCharArray();
-
-            // Sort the char array
-            Arrays.sort(charArray);
-            Arrays.sort(charArray1);
-
-            // Create a new string from the sorted array
-            String sortedString = new String(charArray);
-            String sortedString2 = new String(charArray1);
-
-            Set<String> set1 = new HashSet<>(Arrays.asList(sortedString));
-            Set<String> set2 = new HashSet<>(Arrays.asList(sortedString2));
-
-            if(set1.equals(set2)){
-                return true;
-            }
-
-        }
-        else{
+        if (word1.length() != word2.length()) {
             return false;
         }
-        return false;
+
+        // Count character frequencies in both words
+        Map<Character, Integer> freqMap1 = new HashMap<>();
+        Map<Character, Integer> freqMap2 = new HashMap<>();
+
+        for (char c : word1.toCharArray()) {
+            freqMap1.put(c, freqMap1.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : word2.toCharArray()) {
+            freqMap2.put(c, freqMap2.getOrDefault(c, 0) + 1);
+        }
+
+        // Check if the set of character frequencies is the same
+        Set<Integer> freqSet1 = new HashSet<>(freqMap1.values());
+        Set<Integer> freqSet2 = new HashSet<>(freqMap2.values());
+
+        return freqSet1.equals(freqSet2) && freqMap1.keySet().equals(freqMap2.keySet());
+
     }
 }
