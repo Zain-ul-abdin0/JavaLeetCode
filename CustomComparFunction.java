4300497a4442c0b82d2398c2cb34244a9771ab1b
Zain-ul-Abideen <ahmza147@gmail.com>
2024-10-13 23:03:07 +0200

    Comparator DOne

---
+++ CustomComparFunction.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+
+public class CustomComparFunction implements Comparable<Student> {
+    @Override
+    public  int compareTo(Student student) {
+        return 0;
+    }
+}
--- InterviewPreparationJava.java
+++ InterviewPreparationJava.java
@@ -1,19 +1,20 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class InterviewPreparationJava {
     public static void main(String[] args){
-        LinkedList<Integer> links = new LinkedList<Integer>(Arrays.asList(1, 2, 3));
-         List<Integer> results;
-         results = links.stream().filter(f-> f==3).collect(Collectors.toList());
-        System.out.println(results);
-       System.out.println(function());
+        Student student1 = new Student(25,"Zain");
+        Student student2 = new Student(20,"Alex");
+
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(student1);
+        students.add(student2);
+        Collections.sort(students);
+
+        for(Student student : students){
+            System.out.println(student);
+        }
     }
-    public static String function(){
-        return "Hello, world!";
-    }
+
 
 }
